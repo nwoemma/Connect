d8a2a394c625cc2f6170c9151e93062105e0ca26
unknown <nwosuemmanuel159@gmail.com>
2025-06-25 14:40:55 +0100

    last commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,10 +6,11 @@ ALLOWED_HOSTS = ["www.ewapp.com", "ewapp.com","connect-cp0f.onrender.com"]
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default="postgresql://ewapp_user:nBXo6rooo5tchGzGSWiDzza5p35wDh7v@dpg-d1dhr7idbo4c73du05e0-a.frankfurt-postgres.render.com:5432/ewapp_db",
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'Connect/staticfiles')
