1829015cdf40cf63d28ef2ec8ca066888e573cd1
unknown <nwosuemmanuel159@gmail.com>
2025-06-30 13:42:18 +0100

    corrected views and urls

--- api/urls.py
+++ api/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("chats/history/", views.get_chat_history, name="chat_history"),
     path("chats/get_chat_session/", views.get_chat_session, name="get_chat_session"),
     path('users/activate/', views.activate_user),
+    path('users/user_profile/', views.user_profile, name='user_profile'),
+    path('users/update_profile/', views.update_user_profile, name='update_profile'),
 ]
