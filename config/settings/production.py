62b617072e9d6f7b1c6bb1282b8060d3cc1d18f6
unknown <nwosuemmanuel159@gmail.com>
2025-06-30 23:33:32 +0100

    smiles commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import os
 import dj_database_url
 
 DEBUG = True
-ALLOWED_HOSTS = ["www.ewapp.com","127.0.0.1", "ewapp.com","connect-d9z6.onrender.com",]
+ALLOWED_HOSTS = ["www.egapp.com","127.0.0.1", "ewapp.com","connect-d9z6.onrender.com",]
 
 DATABASES = {
     'default': dj_database_url.config(
